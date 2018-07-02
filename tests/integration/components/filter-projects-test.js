21ae975062c74dec9858e3390245056485437f9c
Ryan Achten <ryanachten@gmail.com>
2018-07-02 16:42:04 +1200

    Add integration test for filter-projects component

--- tests/integration/components/filter-projects-test.js
+++ tests/integration/components/filter-projects-test.js
@@ -1,26 +1,70 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import {resolve} from 'rsvp';
+
+const ITEMS = [{category: 'animation'}, {category: 'film'}, {category: 'motion graphics'}];
+const FILTERED_ITEMS = [{category: 'animation'}];
 
 module('Integration | Component | filter-projects', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('Should show all projects by default', async function (assert) {
+    // Want actions to return promises
+    // Since potentially fetching data asynchronously
+    this.set('filterByCategory', () => resolve({ results: ITEMS }));
 
-    await render(hbs`{{filter-projects}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+    // Render component using test data
     await render(hbs`
-      {{#filter-projects}}
-        template block text
+      {{#filter-projects filter=(action filterByCategory) as |results|}}
+        <ul>
+          {{#each results as |item|}}
+            <li class="category">
+              {{item.category}}
+            </li>
+          {{/each}}
+        </ul>
+      {{/filter-projects}}
+    `);
+    return settled().then( () => {
+      assert.equal(this.element.querySelectorAll('.category').length, 3);
+      assert.equal(this.element.querySelector('.category').textContent.trim(), 'animation');
+    });
+  });
+
+  test('Should update with matching listings', async function (assert) {
+
+    this.set('filterByCategory', (val) => {
+      if (val === '') {
+        return resolve({
+          query: val,
+          results: ITEMS });
+      }else{
+        return resolve({
+          query: val,
+          results: FILTERED_ITEMS });
+      }
+    });
+
+    await render(hbs`
+      {{#filter-projects filter=(action filterByCategory) as |filteredResults|}}
+        <ul>
+          {{#each filteredResults as |item|}}
+            <li class="category">
+              {{item.category}}
+            </li>
+          {{/each}}
+        </ul>
       {{/filter-projects}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    // Click on the animation radio input
+    await click('.filter__check--animation');
+
+    return settled().then( () => {
+      assert.equal(this.element.querySelectorAll('.category').length, 1, '1 result returned');
+      assert.equal(this.element.querySelector('.category').textContent.trim(), 'animation');
+    });
   });
 });
