b962cd937f858f43347a69857bd20856e7d3fe22
Ryan Achten <ryanachten@gmail.com>
2018-07-02 18:08:01 +1200

    Add acceptance test for project filtering

--- app/models/project.js
+++ app/models/project.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   featured: DS.attr(),
   title: DS.attr(),
+  category: DS.attr(),
   client: DS.attr(),
   date: DS.attr(),
   image: DS.attr(),
--- tests/acceptance/list-projects-test.js
+++ tests/acceptance/list-projects-test.js
@@ -18,6 +18,18 @@ module('Acceptance | list projects', function(hooks) {
   test('Should should link to project video when project is clicked', async function (assert) {
   });
 
+  test('Should filter projects based on category', async function (assert) {
+    await visit('/work');
+    await click('.filter__check--animation');
+    assert.ok(this.element.querySelector('.project-item__category').textContent.includes('animation'), 'Listing should have a category of animation');
+  });
+
+  test('Should link to Work page', async function (assert) {
+    await visit('/');
+    await click('.menu__work');
+    assert.equal( currentURL(), '/work', 'Should navigate to Work page' );
+  });
+
   test('Should link to About page', async function (assert) {
     await visit('/');
     await click('.menu__about');
