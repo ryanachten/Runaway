c6a553ce538a38db983090ffe16705c97858f220
Ryan Achten <ryanachten@gmail.com>
2018-07-03 19:08:56 +1200

    Add response message action after contact form submission

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -10,9 +10,21 @@ export default Controller.extend({
   isValidEmail: match('emailAddress', /^.+@.+\..+$/),
 
   submitDisabled: computed('fullName', 'isValidEmail', 'message', function () {
-
     // Only return false if the all of the fields have been filled out
     // and if the email input is a valid email address pattern
     return !this.get('isValidEmail') || this.get('fullName') === '' || this.get('message') === '';
   }),
+
+  responseMessage: '',
+
+  actions: {
+    sendMessage(){
+      const response = `Thanks for your interest! We'll get back to you at ${this.get('emailAddress')}`;
+      alert(response);
+      this.set('responseMessage', response);
+      this.set('fullName', '');
+      this.set('emailAddress', '');
+      this.set('message', '');
+    }
+  }
 });
