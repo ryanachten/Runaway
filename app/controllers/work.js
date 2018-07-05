39a6eb6fd9103f56e04f6f65178fb08526d1e445
Ryan Achten <ryanachten@gmail.com>
2018-07-05 20:01:06 +1200

    Add emberfire filtering to work/index query

--- app/controllers/work.js
+++ app/controllers/work.js
@@ -2,16 +2,16 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    filterByCategory(param){
-      if (param !== '') {
+    filterByCategory(category){
+      if (category !== '') {
         return this.get('store')
-          .query('project', {category: param})
+          .query('project', {orderBy: 'category', equalTo: category})
           .then( (results) => {
-            return { query: param, results: results }
+            return { query: category, results: results }
           });
       }else{
         return this.get('store').findAll('project')
-          .then( (results) => { return { query: param, results: results }});
+          .then( (results) => { return { query: category, results: results }});
       }
     }
   }
