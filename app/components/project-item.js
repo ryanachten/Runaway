00f6d9104f03f89e6aa0cbea0ace586b4892f045
Ryan Achten <ryanachten@gmail.com>
2018-07-09 19:55:20 +1200

    Move video.js controllers from template into component js

--- app/components/project-item.js
+++ app/components/project-item.js
@@ -2,9 +2,23 @@ import Component from '@ember/component';
 
 export default Component.extend({
   isWide: false,
+  src: '/video/test1.mp4',
+  controls: true,
   actions: {
-    toggleImageSize: function () {
-      this.toggleProperty('isWide');
+    canplay() {
+      console.log('video ready');
+    },
+
+    ended() {
+      console.log('video ended');
+    },
+
+    pause() {
+      console.log('video is paused');
+    },
+
+    playing() {
+      console.log('video is playing');
     }
   }
 });
