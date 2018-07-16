0f214acc5e4ddba16d8f8bc5af525de37d4f3f43
Ryan Achten <ryanachten@gmail.com>
2018-07-16 20:14:36 +1200

    Update index route to iterate through model projects

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,36 @@
 import Route from "@ember/routing/route";
 
 export default Route.extend({
+  currentProject: null,
+  projects: null,
+  currentIndex: 0,
+
   model(){
-    return this.get('store').findAll('project');
-  }
+    const model = this.get('store').findAll('project');
+    model.then( (modelClass) => {
+      const projects = modelClass.map( (project) => {
+        return project.data;
+      });
+      this.set('projects', projects);
+    })
+    return model;
+  },
+
+  afterModel(){
+    setInterval(() => {
+      this.incrementProject();
+    }, 1000);
+  },
+
+  incrementProject(){
+    const projects = this.get('projects');
+    const currentIndex = this.get('currentIndex');
+    if (currentIndex+1 < projects.length) {
+      this.set('currentIndex', currentIndex+1);
+    }
+    else{
+      this.set('currentIndex', 0);
+    }
+
+  },
 });
