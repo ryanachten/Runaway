a68b7ad86105c229e04bb1913915b91c67c6ddae
Ryan Achten <ryanachten@gmail.com>
2018-07-31 19:25:50 +1200

    Move auth logic from route to controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,46 @@
 import Controller from '@ember/controller';
+import {inject} from '@ember/service';
 
 export default Controller.extend({
+  session: inject(),
+
   email: null,
   password: null,
+  errorMessage: null,
+
+  beforeModel() {
+    return this.get('session').fetch().catch(function () {});
+  },
+
+  setErrorMessage(error){
+    if (error.code === 'auth/invalid-email') {
+      this.set('errorMessage', 'Oops, please check your email');
+    }
+    else{
+      this.set('errorMessage', error.message);
+    }
+  },
+
+  resetErrorMessage(){
+    this.set('errorMessage', null);
+  },
+  
+  actions: {
+    signIn: function (email, password) {
+      console.log(this.get('errorMessage'));
+      this.get('session').open('firebase', {
+        provider: 'password',
+        email: email, //test@example.com
+        password: password, //password123
+      }).then( (arg) => {
+        this.resetErrorMessage();
+      }).catch( (e) => {
+        this.setErrorMessage(e);
+      });
+    },
+    signOut: function () {
+      this.get('session').close();
+    },
+  },
+
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,23 +1,5 @@
 import Route from '@ember/routing/route';
-import {inject} from '@ember/service';
 
 export default Route.extend({
-  session: inject(),
 
-  beforeModel() {
-    return this.get('session').fetch().catch(function () {});
-  },
-
-  actions: {
-    signIn: function (email, password) {
-      this.get('session').open('firebase', {
-        provider: 'password',
-        email: email,
-        password: password,
-      });
-    },
-    signOut: function () {
-      this.get('session').close();
-    },
-  },
 });
