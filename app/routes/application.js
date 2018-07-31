d282c85b41d813a7f40b405ee917f5378aca7b7c
Ryan Achten <ryanachten@gmail.com>
2018-07-31 19:44:03 +1200

    Oops bad idea, move auth logic back to route (so auth persists), Set controller error msg via route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,40 @@
 import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
 
 export default Route.extend({
+  session: inject(),
 
+  beforeModel() {
+    return this.get('session').fetch().catch(function () {});
+  },
+
+  setErrorMessage(error){
+    if (error.code === 'auth/invalid-email') {
+      this.controller.set('errorMessage', 'Oops, please check your email');
+    }
+    else{
+      this.controller.set('errorMessage', error.message);
+    }
+  },
+
+  resetErrorMessage(){
+    this.controller.set('errorMessage', null);
+  },
+
+  actions: {
+    signIn: function (email, password) {
+      this.get('session').open('firebase', {
+        provider: 'password',
+        email: email,
+        password: password,
+      }).then( (arg) => {
+        this.resetErrorMessage();
+      }).catch( (e) => {
+        this.setErrorMessage(e);
+      });
+    },
+    signOut: function () {
+      this.get('session').close();
+    },
+  },
 });
