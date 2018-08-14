02ee3cc6c096dfe6d5baadd55b2959b7f52ac12f
Ryan Achten <ryanachten@gmail.com>
2018-08-14 19:44:24 +1200

    Add conditional class bindings for navbar on index page

--- app/components/navbar-header.js
+++ app/components/navbar-header.js
@@ -1,6 +1,15 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
 
 export default Component.extend({
+
+  classNameBindings: ['isIndex'],
+
+  isIndex: computed('route', function () {
+    const route = this.get('route') === 'index' ? 'isIndex' : '';
+    return route;
+  }),
+
   actions: {
     signOut: function () {
       this.get('session').close();
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,12 @@
 import Controller from '@ember/controller';
+import {computed} from '@ember/object';
 
 export default Controller.extend({
   email: null,
   password: null,
   errorMessage: null,
 
+  route: computed('currentRouteName', function () {
+    return this.get('currentRouteName');
+  }),
 });
