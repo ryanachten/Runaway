ce75e030c5641c836e4283d6e45765a1488546b8
Ryan Achten <ryanachten@gmail.com>
2018-08-16 19:03:33 +1200

    Merge project item auth/unauth templates, Add edit project toggle functionality

--- app/components/project-item.js
+++ app/components/project-item.js
@@ -1,10 +1,49 @@
 import Component from '@ember/component';
+import {inject} from '@ember/service';
+import {computed} from '@ember/object';
 
 export default Component.extend({
+  store: inject(),
+
   videoPlayer: null,
   videoElement: null,
+  isEditing: false,
+  authed: false,
+
+  canEdit: computed('isEditing', 'authed', function () {
+    const isEditing = this.get('isEditing');
+    const authed = this.get('authed');
+    return authed && isEditing;
+  }),
 
   actions: {
+
+    toggleEditProject(){
+      const isEditing = this.get('isEditing');
+      this.set('isEditing', !isEditing);
+    },
+
+    editProject(project){
+      const store = this.get('store');
+      store.findRecord('project', project.id, { backgroundReload: false }).then((record) => {
+        record.set('title', project.title);
+        record.set('client', project.client);
+        record.set('date', project.date);
+        record.set('category', project.category);
+        record.set('description', project.description);
+        record.save();
+      });
+    },
+
+    deleteProject(id){
+      const store = this.get('store');
+      store.findRecord('project', id, { backgroundReload: false }).then((record) => {
+        record.deleteRecord();
+        record.get('isDeleted');
+        record.save();
+      });
+    },
+
     canplay(player, component) {
       this.set('videoPlayer', player);
       this.set('videoElement', component.get('element'));
