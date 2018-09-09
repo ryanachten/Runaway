69c361f2dcf7e4b97242bbb4b89808955c514ed6
Ryan Achten <ryanachten@gmail.com>
2018-09-09 16:40:35 +1200

    Add liquid fire for route transition animations

---
+++ app/transitions.js
@@ -0,0 +1,30 @@
+export default function(){
+  this.transition(
+    this.toRoute('about'),
+    this.use('explode',
+      animateContainer('.about'),
+    ),
+  );
+
+  this.transition(
+    this.toRoute('contact'),
+    this.use('explode',
+      animateContainer('.contact'),
+    ),
+  );
+
+  this.transition(
+    this.toRoute('work.index'),
+    this.use('explode',
+      animateContainer('.work'),
+    ),
+  );
+}
+
+function animateContainer(container) {
+  const duration = 2000;
+  return({
+    pickNew: container,
+    use: ['fade', {duration}]
+  });
+}
