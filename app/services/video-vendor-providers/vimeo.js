1f18b0e9f09ba54caf348da9e3348c9fe5a37480
Ryan Achten <ryanachten@gmail.com>
2019-03-17 10:56:59 +1300

    Handle get vimeo thumbnail img error properly

--- app/services/video-vendor-providers/vimeo.js
+++ app/services/video-vendor-providers/vimeo.js
@@ -4,20 +4,24 @@
 */
 
 export default {
-  apiUrl(videoId){
+  apiUrl(videoId) {
     return `//vimeo.com/api/oembed.json?url=http%3A//vimeo.com/${videoId}`;
   },
 
-  embedUrl(videoId){
+  embedUrl(videoId) {
     return `//player.vimeo.com/video/${videoId}`;
   },
 
-  thumbnailUrl(videoId){
+  thumbnailUrl(videoId) {
     const apiUrl = this.apiUrl(videoId);
-    return new Promise( (resolve) => {
-      $.getJSON(apiUrl).then((res) => {
-        resolve(res.thumbnail_url);
-      });
+    return new Promise((resolve, reject) => {
+      $.getJSON(apiUrl)
+        .then(res => {
+          resolve(res.thumbnail_url);
+        })
+        .catch(e => {
+          reject("Error getting Vimeo thumbnail", e);
+        });
     });
   }
-}
+};
